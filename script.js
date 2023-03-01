ad55c613f1e166f883f31fa4be1bcd515f6ae05f
MD ARIF <arifahmad207@gmail.com>
2023-03-01 12:57:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+// This function clear all the values
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+ 
+// This function display values
+function display(value) {
+    document.getElementById("result").value += value;
+}
+ 
+// This function evaluates the expression and returns result
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
